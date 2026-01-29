c01c661ca945dc4b74fd647046c49deb4b0087af
Azer Kazimov <azer.kazimov@yahoo.com>
2026-01-29 17:56:55 +0400

    Add location and mapping features to Profile component; update dependencies in package.json and app.json

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,77 @@
-import { Text, View } from "react-native";
+import { Platform, StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as Location from 'expo-location';
+import * as Device from "expo-device";
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { useEffect, useState } from "react";
 
 export default function Profile() {
-    return(
-        <View>
-            <Text>Profile</Text>
-        </View>
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+    useEffect(() => {
+        async function getCurrentLocation() {
+            if (Platform.OS === 'android' && !Device.isDevice) {
+                setErrorMsg("Oops, this will not work on Snack or in the Expo app that comes with React Native!");
+                return;
+            }
+            let { status } = await Location.requestForegroundPermissionsAsync();
+
+            if (status !== "granted") {
+                setErrorMsg("Permission to access location was denied");
+                return;
+            }
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        }
+        getCurrentLocation();
+    }, [])
+
+    if (errorMsg) {
+        return <Text>{errorMsg}</Text>
+    }
+
+    if (!location) {
+        return <Text>Loading...</Text>
+    }
+
+    return (
+        <SafeAreaView>
+            <MapView
+                style={styles.map}
+                provider={PROVIDER_GOOGLE}
+                initialRegion={{
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                }}
+                showsUserLocation={true}
+                followsUserLocation={true}
+                showsMyLocationButton={true}
+                // showsCompass={true}
+                // showsScale={true}
+                // showsTraffic={true}
+                // showsBuildings={true}
+            >
+                <Marker
+                    coordinate={{
+                        latitude: location.coords.latitude,
+                        longitude: location.coords.longitude,
+                    }}
+                    title="You are here"
+                    description={`Latitude: ${location.coords.latitude}, Longitude: ${location.coords.longitude}`}
+                />
+            </MapView>
+
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    map: {
+        width: "100%",
+        height: "100%",
+    }
+
+})
